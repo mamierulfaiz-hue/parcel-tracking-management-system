9b9f2285ab19b7e66b1807cd22872fff8fe91543
Amierul Faiz <m.amierulfaiz@gmail.com>
2026-04-08 00:47:59 +0800

    Add Aiven SSL certificate and config

--- config/database.php
+++ config/database.php
@@ -146,11 +146,9 @@ return [
 
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
-            'persistent' => env('REDIS_PERSISTENT', false),
-        ],
+'options' => [
+    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', base_path('ca.pem')),
+],
 
         'default' => [
             'url' => env('REDIS_URL'),
